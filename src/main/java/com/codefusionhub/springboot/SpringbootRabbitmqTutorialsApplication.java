d11e29fcd1351f43f2f8ea8f2004bff9f898ec86
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-26 08:16:40 -0700

    Initial commit

---
+++ src/main/java/com/codefusionhub/springboot/SpringbootRabbitmqTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.codefusionhub.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefusionhub/springboot/SpringbootRabbitmqTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefusionhub.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
